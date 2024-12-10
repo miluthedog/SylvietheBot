2a7d040e26af81e93496ae55ae4497ecc80bd6eb
Pha <anhphamhoang04@gmail.com>
2024-12-11 04:56:35 +0700

    modified

--- cogs/planner.py
+++ cogs/planner.py
@@ -6,6 +6,8 @@ class planner(commands.Cog):
     def __init__(self, sylvie):
         self.sylvie = sylvie
 
+    @commands.hybrid_command(description="Sylvie find your study docs")
+    async def
 
 
 async def setup(sylvie):
