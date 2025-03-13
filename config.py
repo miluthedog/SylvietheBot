76746be2119b667cde5a82f62d6bf595b39b1357
Do Tran Thai An <justapie06@gmail.com>
2025-03-13 22:30:33 +0700

    feat: change to .env file for easier config

--- config.py
+++ config.py
@@ -1,19 +1,25 @@
-import secret
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+secret = dotenv_values(".env")
 
 moduleList = [
-"module.general",
-"module.StudyTracker",
-"module.TasksManager"
+    "module.general",
+    "module.StudyTracker",
+    "module.TasksManager"
 ]
+
 
 class ID:
     main = 1242439205406244954
     voice = 1249733414601625610
 
     Pha = [
-        754703228003942522,# Pha
-        754703510360162425 # Pha con
+        754703228003942522,  # Pha
+        754703510360162425  # Pha con
     ]
+
 
 class token:
     discord = secret.DISCORDTOKEN
