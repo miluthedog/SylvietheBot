2f09c80967ab9309dbfb9cd5c8c37d38a0f3df1b
Do Tran Thai An <justapie06@gmail.com>
2025-03-14 14:33:00 +0700

    feat: automatically create db dir when not exist

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import discord
 from discord.ext import commands
 import asyncio
 import config
+import os
 
-sylvie = commands.Bot(command_prefix = 'Sylvie ', intents = discord.Intents.all())
+sylvie = commands.Bot(command_prefix='Sylvie ', intents=discord.Intents.all())
+
 
 async def loadModules():
     modules = config.moduleList
     for module in modules:
         await sylvie.load_extension(module)
 
+
 async def SylvieOS():
     APIkey = config.token.discord
+    if not os.path.isdir("./db"):
+        os.mkdir("db")
     async with sylvie:
         await loadModules()
         await sylvie.start(APIkey)
